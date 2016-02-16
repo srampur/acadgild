fc7d2fa438396d3bb8961105f56e07088b854ebc
srampur <srampur@arrisi.com>
2016-02-16 23:51:29 +0530

    Assignment 3

---
+++ Assignment/Assignment3/UnableToStore.java
@@ -0,0 +1,16 @@
+/**
+ * Give two examples of data that cannot be stored in a variable of type int
+ */
+public class UnableToStore
+{
+	public static void main(String args[])
+	{
+		int iNum;
+		long lNum = 40000;
+		double dNum = 41000.4;
+		/* convert long to integer */
+		iNum = lNum;
+		/* convert double to integer */
+		iNum = dNum;
+	}
+}
