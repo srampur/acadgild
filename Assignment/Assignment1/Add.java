124c8a600b6cf84909d4bc4b98c8fa7e3e584ef9
srampur <srampur@arrisi.com>
2016-02-16 23:49:01 +0530

    Assignment 1

---
+++ Assignment/Assignment1/Add.java
@@ -0,0 +1,15 @@
+/**
+ * Write  a  program  to  print  the  sum  of  two  numbers. 
+ * One  number  should  be  int  and  another  should  be float
+ * Output: The sum of two numbers should be in integer.
+ */
+public class Add
+{
+	public static void main(String args[])
+	{
+		int iNum = 10;
+		float fNum = 11.4f;
+		int sum = (int) (iNum + fNum);
+		System.out.println("Sum of two number in integer is "+sum);
+	}
+}
