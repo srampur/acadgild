c3266790116976dbc551083bf8e205cefa90ec5c
srampur <srampur@arrisi.com>
2016-02-16 23:50:29 +0530

    Assignment 2

---
+++ Assignment/Assignment2/Declaration.java
@@ -0,0 +1,11 @@
+/**
+ * Declare three integer variables (a,b,c) in a single declaration and
+ * simultaneously initialize b to 4.
+ */
+public class Declaration
+{
+	public static void main(String args[])
+	{
+		int a,b=4,c;
+	}
+}
